f439f14d19918d00eb345a4924bc835989d7c985
brianhouse <brian.house@gmail.com>
2016-10-31 19:42:06 -0400

    with spinner

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
 
-import os, curses
+import os, curses, itertools, time
 from housepy import config, log
 
-
 messages = []
+spinner = itertools.cycle(['—', '\\', '|', '/'])
+
+ready = True
 
 def curses_main(args):
+    global ready
     try:
         w = curses.initscr()                                            # initialize
         curses.use_default_colors()                                     # use the terminal settings for color_pair 0, otherwise it's black and white
@@ -17,20 +20,27 @@ def curses_main(args):
         curses.echo()                   # show what's being typed
         while True:
             LINES, COLUMNS = w.getmaxyx()
-            w.addstr(LINES - 1, 0, "> ", curses.color_pair(3))        # display something
-            w.clrtoeol()                        # erase from cursor to end of line
-            s = w.getstr().decode().strip()     # allow typing and return
-            if len(s):
-                messages.append(s)
-            if s == "exit":
-                break
             for i in range(LINES - 1):
                 if i < len(messages):                    
                     message = messages[-1 * (i + 1)]
                     index = messages.index(message)
                     color = 1 if (index % 2) else 2
                     w.addstr(LINES - i - 2, 0, message, curses.A_BOLD | curses.color_pair(color))    # draw the line
-                    w.clrtoeol()                                                                    # erase the rest of it
+                    w.clrtoeol()         # erase the rest of it
+            if ready:
+                curses.curs_set(1)
+                w.addstr(LINES - 1, 0, "> ", curses.color_pair(3))        # display something
+                w.clrtoeol()                        # erase from cursor to end of line                
+                s = w.getstr().decode().strip()     # allow typing and return
+                if len(s):
+                    messages.append(s)
+                if s == "go":
+                    ready = False
+            else:
+                curses.curs_set(0)
+                w.addstr(LINES - 1, 0, "Waiting for response... " + next(spinner), curses.A_REVERSE)
+                w.refresh()
+                time.sleep(0.1)
     except Exception as e:
         log.error(log.exc(e))
 
@@ -38,11 +48,9 @@ curses.wrapper(curses_main)
 
 
 """
-Comment out exit in production
 
+disable control-c?
 
-w.addstr(LINES - i - 2, 0, messages[-1 * (i + 1)], curses.A_REVERSE)  # draw the line
-video reverse colors
-curses.A_BOLD
+it should cache the whole conversation and load it on startup (via an argument)
 
 """
