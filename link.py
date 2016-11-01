6d85952e6aa00fa4316be1588564ba767314a332
brianhouse <brian.house@gmail.com>
2016-11-01 10:00:15 -0400

    adding socket handling

---
+++ link.py
@@ -0,0 +1,48 @@
+import serial, threading, time, os, socket, queue
+from housepy import config, log, util
+
+
+class Receiver(threading.Thread):
+
+    def __init__(self, port=23232, message_handler=None,):
+        super(Receiver, self).__init__()
+        self.daemon = True
+        self.messages = queue.Queue()
+        try:
+            self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            self.socket.bind(('', port))
+        except Exception as e:
+            log.error(log.exc(e))
+            return
+        self.start()
+
+    def run(self):
+        while True:
+            try:
+                message, address = self.socket.recvfrom(1024)
+                self.messages.put(message.decode())
+            except Exception as e:
+                log.error(log.exc(e))
+
+
+class Sender(threading.Thread):
+
+    def __init__(self):
+        super(Sender, self).__init__()
+        self.daemon = True
+        self.messages = queue.Queue()
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.socket.settimeout(1)
+        self.start()
+
+    def run(self):
+        while True:
+            try:
+                message, address = self.messages.get(), ("localhost", 23234)
+                log.info("SENDING [%s] to %s:%s" % (message, address[0], address[1]))
+                self.socket.sendto(message.encode('ascii'), address)
+            except Exception as e:
+                log.error(log.exc(e))
+
+    def send(self, message, address):
+        self.messages.put((message, address))
--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python3
 
-import os, curses, itertools, time
+import os, curses, itertools, time, queue
+import link
 from housepy import config, log
+
+receiver = link.Receiver()
+sender = link.Sender()
 
 messages = []
 spinner = itertools.cycle(['—', '\\', '|', '/'])
-
 ready = True
 
 def curses_main(args):
-    global ready
+    global ready, receiver, sender
     try:
         w = curses.initscr()                                            # initialize
         curses.use_default_colors()                                     # use the terminal settings for color_pair 0, otherwise it's black and white
@@ -17,34 +20,41 @@ def curses_main(args):
         curses.init_pair(2, curses.COLOR_RED, curses.COLOR_WHITE)
         curses.init_pair(3, curses.COLOR_BLACK, curses.COLOR_YELLOW)
         w.bkgd(" ", curses.color_pair(1))                               # use a custom for the default
-        curses.echo()                   # show what's being typed
+        curses.echo()                                                   # show what's being typed
         while True:
             LINES, COLUMNS = w.getmaxyx()
             for i in range(LINES - 1):
                 if i < len(messages):                    
                     message = messages[-1 * (i + 1)]
                     index = messages.index(message)
-                    color = 1 if (index % 2) else 2
-                    w.addstr(LINES - i - 2, 0, message, curses.A_BOLD | curses.color_pair(color))    # draw the line
-                    w.clrtoeol()         # erase the rest of it
+                    color = 1 if message[1] == 0 else 2
+                    w.addstr(LINES - i - 2, 0, message[1], curses.A_BOLD | curses.color_pair(color))    # draw the line
+                    w.clrtoeol()                                                                        # erase the rest of it
             if ready:
                 curses.curs_set(1)
-                w.addstr(LINES - 1, 0, "> ", curses.color_pair(3))        # display something
-                w.clrtoeol()                        # erase from cursor to end of line                
-                s = w.getstr().decode().strip()     # allow typing and return
-                if len(s):
-                    messages.append(s)
-                if s == "go":
-                    ready = False
+                w.addstr(LINES - 1, 0, "> ", curses.color_pair(3))          # display something
+                w.clrtoeol()                                                # erase from cursor to end of line                
+                message_s = w.getstr().decode().strip()                     # allow typing and return
+                if len(message_s):
+                    messages.append((0, message_s))
+                sender.messages.put(message_s)
+                ready = False
             else:
                 curses.curs_set(0)
                 w.addstr(LINES - 1, 0, "Waiting for response... " + next(spinner), curses.A_REVERSE)
                 w.refresh()
                 time.sleep(0.1)
+                try:
+                    message_s = receiver.messages.get_nowait()
+                    messages.append((1, message_s))
+                    ready = True
+                except queue.Empty:
+                    pass
     except Exception as e:
         log.error(log.exc(e))
 
 curses.wrapper(curses_main)
+
 
 
 """
